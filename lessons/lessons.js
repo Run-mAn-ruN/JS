7911adc189e176d9554830ce3bf506b911e11c5d
Vladimir <grebetc98@gmail.com>
2021-07-27 19:08:52 +1000

    меняем функцию на стрелочную

--- lessons/lessons.js
+++ lessons/lessons.js
@@ -1,6 +1,19 @@
 "use strict"
+//меняем функцию на стрелочную
 
 function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    function () { alert("You agreed!"); },
+    function () { alert("We canceled the execution!"); }
+);
+
+
+let ask = (question, yes, no) => {
     if (confirm(question)) yes();
     else no();
 }
